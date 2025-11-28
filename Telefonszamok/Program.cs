7129185292c1cbe47a809a1df3e2670bb4f2b75f
kristofav6584 <kovacsk3@kkszki.hu>
2025-11-28 10:33:23 +0100

    menü elkészült

--- Telefonszamok/Program.cs
+++ Telefonszamok/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+            Console.WriteLine("0. Kilépés");
+            Console.WriteLine("1. Mindent megjelenít");
+            Console.WriteLine("2. Csak a számjegyeket tartalmazók");
+            Console.WriteLine("3. Csak a 12 hosszóak");
+            Console.WriteLine("4. Csak a számjegyeket és + jelet tartalmazhat");
+            Console.WriteLine("5. Csak azok jelenjenek meg amiben 6-os szám van");
+            Console.WriteLine("6. Csak számok és + jel, de csak elől lehet a +jel");
+            Console.WriteLine("7. 06-tal kezdődik");
+            Console.WriteLine("8. ( és vagy ) jel van benne");
+
+            string valaszt = Console.ReadLine();
+            if (valaszt == "0")
+            {
+                return;
+            }
+            else if (valaszt == "1")
+            {
+                //1
+            }
+            else if (valaszt == "2")
+            {
+                //2
+            }
+            else if (valaszt == "3")
+            {
+                //3
+            }
+            else if (valaszt == "4")
+            {
+                //4
+            }
+            else if (valaszt == "5")
+            {
+                //5
+            }
+            else if (valaszt == "6")
+            {
+                //6
+            }
+            else if (valaszt == "7")
+            {
+                //7
+            }
+            else if (valaszt == "8")
+            {
+                //8
+            }
+            Console.ReadKey();
         }
     }
 }
